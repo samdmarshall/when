a91d18eb30c2949f03eee5ec8296da8ba5d004ec
Samantha Marshall <me@samdmarshall.com>
2017-03-27 13:37:43 -0700

    updating when to have color

--- when.nim
+++ when.nim
@@ -1,4 +1,9 @@
+import posix
 import times
+import strutils
+import terminal
+
+addQuitProc(resetAttributes)
 
 let local_time = getLocalTime(getTime())
 
@@ -6,6 +11,26 @@ let day = local_time.format("d")
 let month = local_time.format("MMMM")
 let year = local_time.format("yyyy")
 
-let time = local_time.format("HH:mm")
+let hour = local_time.format("HH")
+let minute = local_time.format("mm")
 
-echo("it is the " & day & " day of " & month & ", " & year & ", at " & time & ".")
+stdout.write("it is the ")
+stdout.setForegroundColor(fgBlue)
+stdout.write(day)
+stdout.resetAttributes()
+stdout.write(" day of ")
+stdout.setForegroundColor(fgBlue)
+stdout.write(month)
+stdout.resetAttributes()
+stdout.write(", ")
+stdout.setForegroundColor(fgBlue)
+stdout.write(year)
+stdout.resetAttributes()
+stdout.write(", at ")
+stdout.setForegroundColor(fgGreen)
+stdout.write(hour)
+stdout.resetAttributes()
+stdout.write(":")
+stdout.setForegroundColor(fgGreen)
+stdout.write(minute)
+stdout.write("\n")
