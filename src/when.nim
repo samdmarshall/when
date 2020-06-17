dd56a6297339a04d48f32249aa416c1733bdffb9
Samantha Demi <hello@pewpewthespells.com>
2020-06-17 02:35:02 -0400

    lost code changes in merge :(

--- src/when.nim
+++ src/when.nim
@@ -1,9 +1,51 @@
+
+# =======
+# Imports
+# =======
+
 import times
 import strutils
 import terminal
+import strformat
+
+import commandeer
+
+# =====
+# Types
+# =====
+
+type
+  ColorFlagOptions = enum
+    Auto,
+    Always,
+    Never
+
+# =========
+# Constants
+# =========
+
+const
+  NimblePkgVersion {.strdefine.} = ""
+  NimblePkgName {.strdefine.} = ""
+
+# =========
+# Templates
+# =========
+
+template color(color: untyped, body: untyped) =
+  block:
+    if display_color != Never:
+      stdout.setForegroundColor(color)
+    body
+    if display_color != Never:
+      stdout.resetAttributes()
+
+# =========
+# Functions
+# =========
 
 proc daySuffix(day: string): string =
-  let day_end_digit = $day[day.high()]
+  let day_end_digit = $(day[day.high()])
   case day_end_digit
   of "1":
     return "st"
@@ -14,39 +56,52 @@ proc daySuffix(day: string): string =
   else:
     return "th"
 
-let local_time = getLocalTime(getTime())
+# ==========
+# Main Entry
+# ==========
 
-let weekday = local_time.format("dddd")
-let day = local_time.format("d")
-let suffix = day.daySuffix()
-let month = local_time.format("MMMM")
-let year = local_time.format("yyyy")
+proc main() =
+  let local_time = local(now())
 
-let hour = local_time.format("HH")
-let minute = local_time.format("mm")
+  let weekday = local_time.format("dddd")
+  let day = local_time.format("d")
+  let suffix = daySuffix(day)
+  let month = local_time.format("MMMM")
+  let year = local_time.format("yyyy")
 
-stdout.write("it is ")
-stdout.setForegroundColor(fgBlue)
-stdout.write(weekday)
-stdout.resetAttributes()
-stdout.write(", the ")
-stdout.setForegroundColor(fgBlue)
-stdout.write(day)
-stdout.write(suffix)
-stdout.resetAttributes()
-stdout.write(" day of ")
-stdout.setForegroundColor(fgBlue)
-stdout.write(month)
-stdout.resetAttributes()
-stdout.write(", ")
-stdout.setForegroundColor(fgBlue)
-stdout.write(year)
-stdout.resetAttributes()
-stdout.write(", at ")
-stdout.setForegroundColor(fgGreen)
-stdout.write(hour)
-stdout.resetAttributes()
-stdout.write(":")
-stdout.setForegroundColor(fgGreen)
-stdout.write(minute)
-stdout.resetAttributes()
+  let hour = local_time.format("HH")
+  let minute = local_time.format("mm")
+
+  commandline:
+    option ColorFlag, string, "color", "c", "auto"
+    exitoption "help", "h", fmt"{NimblePkgName} [-h|--help] [-v|--version] [-c|--color:<auto|always|never>]"
+    exitoption "version", "v", fmt"{NimblePkgName} v{NimblePkgVersion}"
+
+  var display_color = parseEnum[ColorFlagOptions](ColorFlag)
+  if display_color == Auto:
+    display_color =
+      if isatty(stdout): Always
+      else: Never
+
+  stdout.write("it is ")
+  color(fgBlue):
+    stdout.write(weekday)
+  stdout.write(", the ")
+  color(fgBlue):
+    stdout.write(day)
+    stdout.write(suffix)
+  stdout.write(" day of ")
+  color(fgBlue):
+    stdout.write(month)
+  stdout.write(", ")
+  color(fgBlue):
+    stdout.write(year)
+  stdout.write(", at ")
+  color(fgGreen):
+    stdout.write(hour)
+  stdout.write(":")
+  color(fgGreen):
+    stdout.write(minute)
+
+when isMainModule:
+  main()
